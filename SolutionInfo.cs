cfcb6de4fd811e19f776f06e1798d51d455f026a
daleb <dalebarr@gmail.com>
2016-11-18 15:22:18 +1300

    Initial

---
+++ SolutionInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyConfiguration("")]
+
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+//[assembly: ComVisible(true)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version   - This is the Phase number
+//      Minor Version   - This is to be incremented for each release to internal QA when there are major changes
+//      Build Number    - This is to be incremented for each release to internal QA
+//      Revision        - This is to be the SVN revision but for now it will be set by VS
+//
+
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyFileVersion("1.2.4.0")]
+
+//[assembly: SecurityPermission(SecurityAction.RequestMinimum, Execution = true)]
